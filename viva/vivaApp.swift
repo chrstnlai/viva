389156b7d77334911e461353b8a1f8c42c9ebdfb
chrstnlai <christinelai04@gmail.com>
2024-11-19 07:04:06 -0800

    Initial Commit

---
+++ viva/vivaApp.swift
@@ -0,0 +1,17 @@
+//
+//  vivaApp.swift
+//  viva
+//
+//  Created by Christine Lai on 11/19/24.
+//
+
+import SwiftUI
+
+@main
+struct vivaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
